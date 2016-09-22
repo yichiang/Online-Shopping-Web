d7adcc57faf9efe186d0ec19d48c17ca1fb475be
yichiang <yi_chiang@outlook.com>
2016-09-22 13:01:20 -0700

    add data annotations to register view model

--- src/OnlineShoppingWeb/ViewModels/RegisterViewModel.cs
+++ src/OnlineShoppingWeb/ViewModels/RegisterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,20 @@ namespace OnlineShoppingWeb.ViewModels
 {
     public class RegisterViewModel
     {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="Confirm Password")]
+        public string ComfirmPassword { get; set; }
     }
 }
