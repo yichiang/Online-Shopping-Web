7bdb27fb8720a41742792affa983fa3488c03e79
yichiang <yi_chiang@outlook.com>
2016-09-22 12:54:00 -0700

    update database with user identity

--- src/OnlineShoppingWeb/Migrations/20160922195228_v2.Designer.cs
+++ src/OnlineShoppingWeb/Migrations/20160922195228_v2.Designer.cs
@@ -8,9 +8,10 @@ using OnlineShoppingWeb.Enities;
 namespace OnlineShoppingWeb.Migrations
 {
     [DbContext(typeof(ProductDbContext))]
-    partial class ProductDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160922195228_v2")]
+    partial class v2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
