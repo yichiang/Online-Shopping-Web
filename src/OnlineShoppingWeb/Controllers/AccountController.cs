9a31f75d8c8f886232d2d820431683056c39fc3e
yichiang <yi_chiang@outlook.com>
2016-09-22 12:56:54 -0700

    create account controller for register

---
+++ src/OnlineShoppingWeb/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace OnlineShoppingWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
