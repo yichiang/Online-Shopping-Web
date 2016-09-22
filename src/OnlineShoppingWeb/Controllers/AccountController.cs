066bcf3f795751519bba43913ce54e1576a2303b
yichiang <yi_chiang@outlook.com>
2016-09-22 13:19:16 -0700

    update form validation for user registerion

--- src/OnlineShoppingWeb/Controllers/AccountController.cs
+++ src/OnlineShoppingWeb/Controllers/AccountController.cs
@@ -1,15 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShoppingWeb.Enities;
+using OnlineShoppingWeb.ViewModels;
+using System.Threading.Tasks;
 
 namespace OnlineShoppingWeb.Controllers
 {
     public class AccountController : Controller
     {
+        private SignInManager<User> _signInManager;
+        private UserManager<User> _userManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         // GET: /<controller>/
         [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                var user = new User { UserName = model.Username, Email = model.Email };
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            
+           return View();
+         
         }
     }
 }
