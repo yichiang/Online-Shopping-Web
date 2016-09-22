732d436feeeb9d540d0105bfd8e4300b9cef4515
yichiang <yi_chiang@outlook.com>
2016-09-22 11:37:52 -0700

    add dbcontext and sqldata

--- src/OnlineShoppingWeb/Enities/ProductDbContext.cs
+++ src/OnlineShoppingWeb/Enities/ProductDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OnlineShoppingWeb.Enities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,6 @@ namespace OnlineShoppingWeb.Models
 {
     public class ProductDbContext : DbContext
     {
+        public DbSet<Laptop> Laptops { get; set; }
     }
 }
---
+++ src/OnlineShoppingWeb/Services/SqlserverLaptopData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OnlineShoppingWeb.Enities;
+using OnlineShoppingWeb.Models;
+
+namespace OnlineShoppingWeb.Services
+{
+
+    public class SqlServerLaptopData : IProductData
+    {
+        private ProductDbContext _context;
+
+        public SqlServerLaptopData(ProductDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddNewProduct(IProduct newProduct)
+        {
+            _context.Add(newProduct);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<IProduct> GetAll()
+        {
+            return _context;
+        }
+
+        public IProduct GetLaptopById(int id)
+        {
+            return _context.Laptops.FirstOrDefault(computer => computer.LaptopId == id );
+        }
+    }
+}
