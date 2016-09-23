fc12bdb94d84cc7989e05d5481e94fdcc1818500
yichiang <yi_chiang@outlook.com>
2016-09-22 21:02:25 -0700

    apply shopping order relationship in user

--- src/OnlineShoppingWeb/Enities/User.cs
+++ src/OnlineShoppingWeb/Enities/User.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-
+using System;
+using System.Collections.Generic;
 
 namespace OnlineShoppingWeb.Enities
 {
     public class User :IdentityUser
     {
+        public string Address { get; set; }
+        public DateTime JoinDate { get; set; }
+        public virtual ICollection<ShoppingOrder> ShoppingOrders { get; set; }
 
     }
 }
