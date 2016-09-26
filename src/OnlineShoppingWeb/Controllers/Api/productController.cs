e14c16499d6079a77aec04defd6196e99b82ac93
yichiang <yi_chiang@outlook.com>
2016-09-26 10:19:38 -0700

    fix display on homw page

---
+++ src/OnlineShoppingWeb/Controllers/Api/productController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShoppingWeb.Enities;
+using OnlineShoppingWeb.Services;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OnlineShoppingWeb.Controllers.Api
+{
+    public class ProductController : Controller
+    {
+        private IProductData _LaptopData;
+
+        public ProductController(IProductData LaptopData)
+        {
+            _LaptopData = LaptopData;
+        }
+
+        [HttpGet("api/product")]
+        public JsonResult Get()
+        {
+            return Json(_LaptopData.GetAll());
+        }
+    }
+}
