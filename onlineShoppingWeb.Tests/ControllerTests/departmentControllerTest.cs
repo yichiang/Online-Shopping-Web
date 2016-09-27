ab5c36c8dd35d8123d23be06ddd2308c6f3cedf4
yichiang <yi_chiang@outlook.com>
2016-09-27 14:50:13 -0700

    run first test

---
+++ onlineShoppingWeb.Tests/ControllerTests/departmentControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShoppingWeb.Controllers;
+using OnlineShoppingWeb.Enities;
+using Xunit;
+namespace onlineShoppingWeb.Tests.ControllerTests
+{   
+    public class DepartmentControllerTest
+    {
+ 
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+ 
+        }
+    }
+}
