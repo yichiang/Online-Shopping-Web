752a8cd62b57d828c3bde5bafc96071a17a39c68
yichiang <yi_chiang@outlook.com>
2016-09-30 12:35:05 -0700

    apply simple jquery to edit form

---
+++ src/OnlineShoppingWeb/wwwroot/js/DepartmentIndex.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $(".editFormForDepartment").addClass("hideAction");
+    $(".editButtonForDepartment").click(function () {
+        if ($(".editFormForDepartment").hasClass("hideAction")) {
+            $(".editFormForDepartment").removeClass("hideAction");
+
+        } else {
+            $(".editFormForDepartment").addClass("hideAction");
+        }
+    });
+});
