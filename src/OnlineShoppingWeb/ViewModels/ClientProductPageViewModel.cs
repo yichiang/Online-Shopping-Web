f68e78017e30a00fafc2417269de58e89e035d89
yichiang <yi_chiang@outlook.com>
2016-10-07 14:25:47 -0700

    refactor client product view model

--- src/OnlineShoppingWeb/ViewModels/ClientProductPageViewModel.cs
+++ src/OnlineShoppingWeb/ViewModels/ClientProductPageViewModel.cs
@@ -4,34 +4,18 @@ using System.Collections.Generic;
 
 namespace OnlineShoppingWeb.ViewModels
 {
-    public class ClientProductPageViewModel
+    public class ClientProductPageViewModel : ProductPageViewModel
     {
 
-        public ClientProductPageViewModel()
+        public ClientProductPageViewModel() : base()
         {
-            IsListAreaVisible = true;
-            IsSearchAreaVisible = true;
             IsDetailAreaVisible = false;
-            TakeDisplayList = 5;
-            SkipDisplayList = 0;
-            EventCommand = "list";
         }
-        public Laptop Laptop { get; set; }
-        public Phone Phone { get; set; }
-        public Product Product { get; set; }
-        public string ProductSearchName { get; set; }
-        public IEnumerable<Product> Products { get; set; }
 
-        public string EventCommand { get; set; }
-        public bool IsListAreaVisible { get; set; }
-        public bool IsSearchAreaVisible { get; set; }
 
         public bool IsDetailAreaVisible { get; set; }
-
         public int SaveToCartProductId { get; set; }
         public string ProductType { get; set; }
-        public int AllProductsCount { get; set; }
-        public int TakeDisplayList { get; set; }
-        public int SkipDisplayList { get; set; }
+   
     }
 }
