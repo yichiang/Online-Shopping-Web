7b476feaa2cdb730abcf0ba1ad1328f09c620870
yichiang <yi_chiang@outlook.com>
2016-10-07 15:22:40 -0700

    remove script tag to its own file

---
+++ src/OnlineShoppingWeb/wwwroot/js/productView.js
@@ -0,0 +1,28 @@
+﻿
+        $(document).ready(function () {
+            //window.scrollTo(0, parseInt($("#windowPostiton").val()));
+            $(".own-btn-action").click(function (e) {
+                e.preventDefault();
+                $("#EventCommand").val($(this).val());
+                $("form.SearchForm").submit();
+            });
+
+            $(".pageButton").click(function () {
+                var pageRange = $(this).text();
+                console.log(pageRange);
+                if (pageRange === "All") {
+                    console.log("All");
+
+                    $("#TakeDisplayList").val($("#AllProductsCount").val());
+                    $("#SkipDisplayList").val("0");
+
+                } else {
+                    var tempArray = pageRange.split("-");
+                    console.log(tempArray);
+                    $("#TakeDisplayList").val("5");
+                    $("#SkipDisplayList").val(parseInt(tempArray) - 1);
+                }
+                $("form.addMoretoListForm").submit();
+
+            });
+        });
