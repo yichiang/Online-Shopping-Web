796e9bba8cbc005aa06e5f158b1f6d043442737f
yichiang <yi_chiang@outlook.com>
2016-10-11 10:52:29 -0700

    add migrations for changing date type of reviews

---
+++ src/OnlineShoppingWeb/Migrations/20161011175149_v3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineShoppingWeb.Migrations
+{
+    public partial class v3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PostTime",
+                table: "ProductReviews",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Score",
+                table: "ProductReviews",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PostTime",
+                table: "ProductReviews");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Score",
+                table: "ProductReviews",
+                nullable: false);
+        }
+    }
+}
