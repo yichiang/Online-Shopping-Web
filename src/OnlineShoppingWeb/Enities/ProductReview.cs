8df4f3c391bd55aee2b7b6c7fa71c763d179b918
yichiang <yi_chiang@outlook.com>
2016-10-11 09:31:02 -0700

    create review schema

---
+++ src/OnlineShoppingWeb/Enities/ProductReview.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineShoppingWeb.Enities
+{
+    [Table("ProductReviews")]
+
+    public class ProductReview
+    {
+        [Key]
+        public int ReviewId { get; set; }
+        [Range(0, 5)]
+        public int Score { get; set; }
+        public string Title { get; set; }
+        public string Context { get; set; }
+
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public int ProductId { get; set; }
+        public virtual Product Proudct { get; set; }
+    }
+}
