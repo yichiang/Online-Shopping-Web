c0b099991d740a53e66496bcb11bda93c17d3275
yichiang <yi_chiang@outlook.com>
2016-10-12 15:25:50 -0700

    add orderitem class to dbcontext

--- src/OnlineShoppingWeb/Enities/OrderItem.cs
+++ src/OnlineShoppingWeb/Enities/OrderItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,8 @@ namespace OnlineShoppingWeb.Enities
 {
     public class OrderItem
     {
+        [Key]
+
         public int Qty { get; set; }
         public int ProductId { get; set; }
         public decimal CurrentPrice { get; set; }
