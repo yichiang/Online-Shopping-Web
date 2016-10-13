f6cc068a46886e27112a8b0681eb0a239256cdb9
yichiang <yi_chiang@outlook.com>
2016-10-12 17:02:47 -0700

    add migration for order item table and order

---
+++ src/OnlineShoppingWeb/Migrations/20161013000228_v2.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineShoppingWeb.Migrations
+{
+    public partial class v2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_ShoppingOrder_ShoppingOrderOrderId",
+                table: "OrderItem");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItem_ShoppingOrderOrderId",
+                table: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "OrderConfirmation",
+                table: "ShoppingOrder");
+
+            migrationBuilder.DropColumn(
+                name: "ShoppingOrderOrderId",
+                table: "OrderItem");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ShoppingOrderId",
+                table: "OrderItem",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItem_ShoppingOrderId",
+                table: "OrderItem",
+                column: "ShoppingOrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_ShoppingOrder_ShoppingOrderId",
+                table: "OrderItem",
+                column: "ShoppingOrderId",
+                principalTable: "ShoppingOrder",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_ShoppingOrder_ShoppingOrderId",
+                table: "OrderItem");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItem_ShoppingOrderId",
+                table: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "ShoppingOrderId",
+                table: "OrderItem");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OrderConfirmation",
+                table: "ShoppingOrder",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ShoppingOrderOrderId",
+                table: "OrderItem",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItem_ShoppingOrderOrderId",
+                table: "OrderItem",
+                column: "ShoppingOrderOrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_ShoppingOrder_ShoppingOrderOrderId",
+                table: "OrderItem",
+                column: "ShoppingOrderOrderId",
+                principalTable: "ShoppingOrder",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
