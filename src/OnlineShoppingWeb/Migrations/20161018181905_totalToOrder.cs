aa784fe95e8d7447f0f7b1e34dfb71d5c70c761a
yichiang <yi_chiang@outlook.com>
2016-10-18 11:19:35 -0700

    add migrations

---
+++ src/OnlineShoppingWeb/Migrations/20161018181905_totalToOrder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineShoppingWeb.Migrations
+{
+    public partial class totalToOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Total",
+                table: "ShoppingOrder",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItem_ProductId",
+                table: "OrderItem",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_Products_ProductId",
+                table: "OrderItem",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_Products_ProductId",
+                table: "OrderItem");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItem_ProductId",
+                table: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "Total",
+                table: "ShoppingOrder");
+        }
+    }
+}
