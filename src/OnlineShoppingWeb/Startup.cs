c4969f1bc88cf399c5f047afb209df029024ffd1
yichiang <yi_chiang@outlook.com>
2016-10-19 08:59:50 -0700

    modify publish require

--- src/OnlineShoppingWeb/Startup.cs
+++ src/OnlineShoppingWeb/Startup.cs
@@ -26,13 +26,22 @@ namespace OnlineShoppingWeb
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json");
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            if (env.IsDevelopment())
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
             Configuration = builder.Build();
         }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry(Configuration);
+
             services.AddMvc().AddJsonOptions(config => config.SerializerSettings.ContractResolver=new CamelCasePropertyNamesContractResolver());
             services.AddEntityFramework()
                 .AddDbContext<ProductDbContext>(options =>
@@ -61,12 +70,21 @@ namespace OnlineShoppingWeb
             Mapper.Initialize(config => { config.CreateMap<IProduct, Laptop>().ReverseMap(); });
             app.UseIdentity();
 
-            loggerFactory.AddConsole();
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            app.UseApplicationInsightsRequestTelemetry();
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseApplicationInsightsExceptionTelemetry();
+
             app.UseFileServer();
             app.UseStaticFiles();
             app.UseNodeModules(env);
